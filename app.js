12d61b02706131308e80838081453266aa319cc1
dLuxKid <adetunjimarvellous09@gmail.com>
2023-12-25 21:43:01 +0100

    refactor: moved handler functions to seperate functions to improve code redability

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
-
 app.use(express.json()); // middleware
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     // using jsend format for sending response from apis
     status: "success",
     results: tours.length,
     data: { tours },
   });
-});
+};
 
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTourById = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((tour) => tour.id === id);
 
@@ -33,9 +32,9 @@ app.get("/api/v1/tours/:id", (req, res) => {
     status: "success",
     data: { tour },
   });
-});
+};
 
-app.post("/api/v1/tours", (req, res) => {
+const addNewTour = (req, res) => {
   const newId = tours[tours.length - 1] + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -53,9 +52,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
 
   if (id >= tours.length) {
@@ -71,9 +70,9 @@ app.patch("/api/v1/tours/:id", (req, res) => {
       tour: "tour has been updated",
     },
   });
-});
+};
 
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   const id = req.params.id * 1;
 
   if (id >= tours.length) {
@@ -95,7 +94,20 @@ app.delete("/api/v1/tours/:id", (req, res) => {
       });
     }
   );
-});
+};
+
+// app.get("/api/v1/tours", getAllTours);
+// app.post("/api/v1/tours", addNewTour);
+app.route("/api/v1/tours").get(getAllTours).post(addNewTour);
+
+app
+  .route("/api/v1/tours/:id")
+  .get(getTourById)
+  .patch(updateTour)
+  .delete(deleteTour);
+// app.get("/api/v1/tours/:id", getTourById);
+app.patch("/api/v1/tours/:id", updateTour);
+app.delete("/api/v1/tours/:id", deleteTour);
 
 app.listen(3000, () => {
   console.log("We on some express juice");
